72c0562d76ffd481bd95363ff19adfacf20df785
Affaan Nadeem <mnanhindi@gmail.com>
2024-11-19 00:13:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Open Modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+// Close Modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close Modal by Clicking Outside
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
